e2f8e209b4e79c083b2b6c8cc79176fe49eeeef1
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-05-15 14:04:29 -0500

    add auth factory

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,13 +1,52 @@
 var express = require('express');
+var jwt = require('express-jwt');
 var router = express.Router();
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
 var mongoose = require('mongoose');
+var passport = require('passport');
+var User = mongoose.model('User');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+});
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
 
 
 
@@ -21,8 +60,9 @@ router.get('/posts', function(req, res, next) {
 });
 
 /* POST posts */
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
 	var post = new Post(req.body); 
+	post.author = req.payload.username; 
 
 	post.save(function(err, post) {
 		if(err) {return next(err);}
@@ -58,7 +98,7 @@ router.get('/posts/:post', function(req, res, next) {
 
 /* upvote post */
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
 	req.post.upvote(function(err, post) {
 		if (err) { return next(err);}
 
@@ -80,9 +120,10 @@ router.param('comment', function(req, res, next, id) {
 });
 /* comments route for particular post */
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth,  function(req, res, next) {
 	var comment = new Comment(req.body);
 	comment.post = req.posts; 
+	comment.author = req.payload.username; 
 
 	comment.save(function(err, comment) {
 		if (err) { return next(err); }
@@ -101,7 +142,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
 
 /* upvote comment */
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
 	req.comment.upvote(function(err, comment) {
 		if(err) { return next(err); }
 
